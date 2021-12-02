a84e8dcf6c8652850b10adfc3024707238cd159f
lucatinti13 <94382916+lucatinti13@users.noreply.github.com>
2021-12-02 18:58:51 +0100

    Add files via upload

--- elabora.c
+++ elabora.c
@@ -21,6 +21,10 @@ int main(int argc, char *argv[]) {											//commento
 	}
 	fh=fopen(argv[1], "r");
 				/*fh=fopen(nome_file, "r");*/
+	if(fh==NULL){
+		printf("Passare un nome file esistente\n");
+		exit(-2);
+	}
 	while (fgets(riga_corrente,sizeof(riga_corrente),fh) != NULL){			// pch=fgets(riga_co}rrente,sizeof(riga_corrente),fh);
 		printf("%s", &(riga_corrente[9]));					//& e 9 - dammi l'indirizzo del carattere con l'avanzamento									//if (pch==NULL) 
 	}									
