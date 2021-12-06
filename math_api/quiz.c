3bb52f300bd8808450edce423289d16b5837b92a
lucatinti13 <94382916+lucatinti13@users.noreply.github.com>
2021-12-06 14:47:12 +0100

    Update quiz.c

--- math_api/quiz.c
+++ math_api/quiz.c
@@ -1,1 +1,14 @@
+#include "quiz.h"
 
+
+int quiz(int x, int y) {
+    int r;
+    if (x < 0) x = -x;
+    if (y < 0) y = -y;
+    while (y) {
+      r = x % y;
+      x = y;
+      Y = r;
+    }
+    return x;
+}
